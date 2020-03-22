c525f6460904cf49c146228352e952b78ff9c3cd
sinatra <is.swag@outlook.com>
2020-03-22 14:04:38 -0500

    Change persistence to Files

--- src/main/java/co/edu/udea/edatos/ejemplo/bsn/ClienteBsn.java
+++ src/main/java/co/edu/udea/edatos/ejemplo/bsn/ClienteBsn.java
@@ -1,12 +1,12 @@
 package co.edu.udea.edatos.ejemplo.bsn;
 
 import co.edu.udea.edatos.ejemplo.dao.ClienteDao;
-import co.edu.udea.edatos.ejemplo.dao.impl.ClienteDAOList;
+import co.edu.udea.edatos.ejemplo.dao.impl.ClienteDaoNio;
 import co.edu.udea.edatos.ejemplo.model.Cliente;
 
 public class ClienteBsn {
 
-    ClienteDao repository = new ClienteDAOList();
+    ClienteDao repository = new ClienteDaoNio();
 
     public void registrarCliente(Cliente cliente){
         repository.guardar(cliente);
--- src/main/java/co/edu/udea/edatos/ejemplo/bsn/EmpleadoBsn.java
+++ src/main/java/co/edu/udea/edatos/ejemplo/bsn/EmpleadoBsn.java
@@ -1,12 +1,12 @@
 package co.edu.udea.edatos.ejemplo.bsn;
 
 import co.edu.udea.edatos.ejemplo.dao.EmpleadoDao;
-import co.edu.udea.edatos.ejemplo.dao.impl.EmpleadoDAOList;
+import co.edu.udea.edatos.ejemplo.dao.impl.EmpleadoDaoNio;
 import co.edu.udea.edatos.ejemplo.model.Empleado;
 
 public class EmpleadoBsn {
 
-    EmpleadoDao repository = new EmpleadoDAOList();
+    EmpleadoDao repository = new EmpleadoDaoNio();
 
     public void registraEmpleado(Empleado empleado) {
         repository.guardar(empleado);
