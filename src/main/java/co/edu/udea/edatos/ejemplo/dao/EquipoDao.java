1e86e2a0ec1a4831f4d6345217c23cd0cd58c73d
sinatra <is.swag@outlook.com>
2020-03-22 18:12:38 -0500

    Create DAOs for Equipo, Factura, Solicitude

--- src/main/java/co/edu/udea/edatos/ejemplo/dao/EquipoDao.java
+++ src/main/java/co/edu/udea/edatos/ejemplo/dao/EquipoDao.java
@@ -1,9 +1,13 @@
 package co.edu.udea.edatos.ejemplo.dao;
 
+import java.util.List;
+
 import co.edu.udea.edatos.ejemplo.model.Equipo;
 
 public interface EquipoDao {
 
     public void guardar(Equipo equipo);
+    public Equipo getEquipoById(int id);
+    public List<Equipo> getEquipoByUserId(int userId);
 
 }
