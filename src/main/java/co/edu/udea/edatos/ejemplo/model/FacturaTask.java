fec1a3e5ba93390a3cc34466bb9eb786c7cc9314
sinatra <is.swag@outlook.com>
2020-04-22 21:00:24 -0500

    [NI004] Create relationship models
    Create FacturaTask and TaskEquipoUser models to allows the relationship
    between Factura with Task as well Task with Equipo and User models.

---
+++ src/main/java/co/edu/udea/edatos/ejemplo/model/FacturaTask.java
@@ -0,0 +1,42 @@
+package co.edu.udea.edatos.ejemplo.model;
+
+public class FacturaTask {
+
+    private int id;
+    private int facturaId;
+    private int taskId;
+
+    public FacturaTask() {
+
+    }
+
+    public FacturaTask(int id, int facturaId, int taskId) {
+        this.id = id;
+        this.facturaId = facturaId;
+        this.taskId = taskId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFacturaId() {
+        return facturaId;
+    }
+
+    public void setFacturaId(int facturaId) {
+        this.facturaId = facturaId;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+}
